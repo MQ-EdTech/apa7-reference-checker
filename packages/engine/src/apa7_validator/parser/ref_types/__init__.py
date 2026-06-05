e994091cbbc498a5ee1873f689281209af938b02
aaron <achakerian@gmail.com>
2026-06-05 23:02:25 +1000

    feat(engine): add book chapter reference parser

--- packages/engine/src/apa7_validator/parser/ref_types/__init__.py
+++ packages/engine/src/apa7_validator/parser/ref_types/__init__.py
@@ -1,4 +1,5 @@
 from .book import try_parse_book
+from .book_chapter import try_parse_book_chapter
 from .journal import try_parse_journal
 
-__all__ = ["try_parse_book", "try_parse_journal"]
+__all__ = ["try_parse_book", "try_parse_book_chapter", "try_parse_journal"]
